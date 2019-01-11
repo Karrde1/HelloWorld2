6ea0b573e5ebfd3a7522b3a6b669cda3c33e3f16
Gregory Gauthier <talonk165@hotmail.com>
2019-01-11 16:59:14 -0500

    Version 2 Hello World

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
+            // Pause To Display Hello World!
+            Console.WriteLine("Press Any Key To Continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+            // Causes Application To End When User Presses Key
         }
     }
 }
